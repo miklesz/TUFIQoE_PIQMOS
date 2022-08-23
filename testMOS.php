06de06d512f203912ef807b7c5d00a487d3114f9
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-08-23 23:56:05 +0200

    Update testMOS.php

--- testMOS.php
+++ testMOS.php
@@ -6,17 +6,16 @@
 	</head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 	<script type="text/javascript" >
-		function checkMos() {	
-			if(atLeastOneRadio()) {
+		function checkMos(buttons) {
+			if(atLeastOneRadio(buttons)) {
 				document.getElementById("nextButton").disabled = false;
 			}
 		}
 		
-		function atLeastOneRadio() {
-			return ($('input[type=radio]:checked').size() > 0);
+		function atLeastOneRadio(buttons) {
+			return ($('input[type=radio]:checked').size() > buttons);
 		}	
-		
-		
+
 		// 	jQuery(document).ready(function($) {
         //
 		// if (window.history && window.history.pushState) {
@@ -29,121 +28,50 @@
         //
 		// }
 		// });
-		
 
 	</script>
 
 <body onload="checkMos()">
 	<div class="answer">
-		<?php 
-		session_start();
-        $mes = 'Assess quality of material displayed';
-        $button = 'Next';
-        $pre_time = microtime(true);
-        $pvs_duration = microtime(true) - $_SESSION['pvs_time']
-        ?>
-		<h3><?php echo $mes; ?></h3>
-<!--        --><?php
-//        echo $_SESSION['pre_time'];
-//        ?>
-		<!--p style=" margin-top: 5px; margin-bottom: 2px; ">Rating</p-->
-		<form method="post" action="next_page.php" style = "margin-bottom: 0;">
+        <form method="post" action="next_page.php" style = "margin-bottom: 0;">
+            <?php
+            session_start();
+            $button = 'Next';
+            $pre_time = microtime(true);
+            $pvs_duration = microtime(true) - $_SESSION['pvs_time'];
+            $quest_correct = $_SESSION['quest_correct'];
+            $pvs_no = $_SESSION['pvs_no'];
+            $buttons = 0;
+            if ($quest_correct[$pvs_no]){
+                $buttons += 1;
+                $quest = substr($quest_correct[$pvs_no], 0, strpos($quest_correct[$pvs_no], ','));
+                echo "<h3>$quest</h3>";
+                echo "<label><input type='radio' name='quest' onClick='checkMos($buttons);' value='true'> true<br><br></label>";
+                echo "<label><input type='radio' name='quest' onClick='checkMos($buttons);' value='false'> false<br><br></label>";
+            }
+//            echo $quest_correct[$pvs_no];
+            echo '<h3>Assess quality of material displayed</h3>';
+            ?>
             <input id="pageNext" type="hidden" name="page" value="mos">
             <input id="pre_time" type="hidden" name="pre_time" value="<?php echo $pre_time; ?>">
             <input id="pvs_duration" type="hidden" name="pvs_duration" value="<?php echo $pvs_duration; ?>">
             <label>
-                <input type="radio" name="mos" onClick="checkMos();" value="1"> bad<br><br>
+                <input type="radio" name="mos" onClick="checkMos(<?php echo $buttons; ?>);" value="1"> bad<br><br>
             </label>
             <label>
-                <input type="radio" name="mos" onClick="checkMos();" value="2"> poor<br><br>
+                <input type="radio" name="mos" onClick="checkMos(<?php echo $buttons; ?>);" value="2"> poor<br><br>
             </label>
             <label>
-                <input type="radio" name="mos" onClick="checkMos();" value="3"> fair<br><br>
+                <input type="radio" name="mos" onClick="checkMos(<?php echo $buttons; ?>);" value="3"> fair<br><br>
             </label>
             <label>
-                <input type="radio" name="mos" onClick="checkMos();" value="4"> good<br><br>
+                <input type="radio" name="mos" onClick="checkMos(<?php echo $buttons; ?>);" value="4"> good<br><br>
             </label>
             <label>
-                <input type="radio" name="mos" onClick="checkMos();" value="5"> excellent<br><br>
+                <input type="radio" name="mos" onClick="checkMos(<?php echo $buttons; ?>);" value="5"> excellent<br><br>
             </label>
             <input id="nextButton" type="submit" value="Next" disabled>
-<!--            --><?php
-//            exit('Exit: testMOS');
-//            ?>
-<!---->
-<!--		--><?php //
-//			if(isset($_SESSION['pageIndex'])) {
-//				?><!--				-->
-<!--					<input id="pageNext" type="hidden" name="page" value=" --><?php //echo  $_SESSION['pageIndex']; ?><!-- ">-->
-<!--				--><?php
-//			}
-//			else {
-//				session_destroy();
-//				header( "Location: ../TUFIQoEMOS/index.php" );
-//			}
-//
-//			if(isset($_SESSION['mosMAX'])) {
-//				$maxMOS = $_SESSION['mosMAX'];
-//			}
-//			else{
-//				$maxMOS = 5;
-//			}
-//
-//			//for ($i = 1; $i <= $maxMOS; $i++) {
-//			for ($i = $maxMOS; $i >= 1; $i--) {
-//				switch($i) {
-//					case 1:
-//						if($lan == 'pl') {
-//							$mos_mes = "zła";
-//						}
-//						else {
-//							$mos_mes = "bad";
-//						}
-//						break;
-//					case 2:
-//						if($lan == 'pl') {
-//							$mos_mes = "słaba";
-//						}
-//						else {
-//							$mos_mes = "poor";
-//						}
-//						break;
-//					case 3:
-//						if($lan == 'pl') {
-//							$mos_mes = "średnia";
-//						}
-//						else {
-//							$mos_mes = "fair";
-//						}
-//						break;
-//					case 4:
-//						if($lan == 'pl') {
-//							$mos_mes = "dobra";
-//						}
-//						else {
-//							$mos_mes = "good";
-//						}
-//						break;
-//					case 5:
-//						if($lan == 'pl') {
-//							$mos_mes = "doskonała";
-//						}
-//						else {
-//							$mos_mes = "excellent";
-//						}
-//						break;
-//					default:
-//						echo "One shall neve get here!";
-//				}
-//				?>
-<!--					<label>-->
-<!--						<input type="radio" name="mos" onClick="checkMos();" value="--><?php //echo $i?><!--"> --><?php //echo $mos_mes?><!--<br><br>-->
-<!--					</label>-->
-<!--				--><?php
-//			}
-//		?><!--		-->
-<!--			<input id="nextButton" type="submit" value="--><?php //echo $button; ?><!--" disabled = true>			-->
-		</form>
+        </form>
 	</div>
 </body>
 </html>
