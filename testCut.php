2fe7b672ea56df05ed1219901485381b225f96d3
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-09-01 23:56:26 +0200

    Update testCut.php

--- testCut.php
+++ testCut.php
@@ -1,6 +1,21 @@
-﻿<?php
+﻿<html lang="">
+<head>
+    <meta charset="iso=8859-2">
+    <title>TUFIQoE PIQMOS psycho-physical test</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<body>
+<?php
 session_start();
-$_SESSION = array();
-session_destroy();
-exit('Exit: testCut');
-//header( "Location: index.php" );
+?>
+<section class="container">
+    <div class="login">
+        <h3 id="com" style="margin-bottom: 5px;">The test is interrupted</h3>
+        <form method="post" action="indexReturn.php">
+            <p class="submit"><input type="submit" name="commit" value="Return"></p>
+        </form>
+    </div>
+</section>
+</body>
+</html>
