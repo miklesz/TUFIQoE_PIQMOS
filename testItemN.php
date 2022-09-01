2e889da57fddf5ba5711cc3f5f26262ce9530a8f
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-09-01 23:56:32 +0200

    Update testItemN.php

--- testItemN.php
+++ testItemN.php
@@ -1,60 +1,39 @@
 ﻿<html lang="">
-	<head>
-		  <meta charset="iso=8859-2">
-		  <title>TUFIQoE PIQMOS psycho-physical test</title>
-		  <title>TUFIQoE PIQMOS psycho-physical test</title>
-		  <link rel="stylesheet" href="css/style_test.css" type="text/css"/>
-	</head>
-	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-    <script type="text/javascript" >
+<head>
+      <meta charset="iso=8859-2">
+      <title>TUFIQoE PIQMOS psycho-physical test</title>
+      <title>TUFIQoE PIQMOS psycho-physical test</title>
+      <link rel="stylesheet" href="css/style_test.css" type="text/css"/>
+</head>
 
-        function time(){
-            // document.getElementById('pvs_duration').innerHTML = (Date.now() - start).toString();
-            localStorage.setItem('pvs_duration', (Date.now() - start).toString());
-            // document.getElementById('pvs_duration').innerHTML = localStorage.getItem('pvs_duration');
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script type="text/javascript" >
+
+    function time(){
+        // document.getElementById('pvs_duration').innerHTML = (Date.now() - start).toString();
+        localStorage.setItem('pvs_duration', (Date.now() - start).toString());
+        // document.getElementById('pvs_duration').innerHTML = localStorage.getItem('pvs_duration');
+    }
+
+    $(window).load(function() {
+        window.setTimeout(function(){
+            window.location.href = "testMOS.php";
+        }, 3000);
+    });
+
+    const start = Date.now();
+    setInterval(time, 1);
+
+    jQuery(document).ready(function($) {
+        if (window.history && window.history.pushState) {
+            window.history.pushState('forward', null, './#forward');
+            $(window).on('popstate', function() {
+                window.location.href = "testCut.php";
+            });
         }
-
-        $(window).load(function() {
-            window.setTimeout(function(){
-                window.location.href = "testMOS.php";
-            }, 3000);
-        });
-
-        const start = Date.now();
-        setInterval(time, 1);
-    </script>
-<!--	<script type="text/javascript" >-->
-<!---->
-<!--		$(document).ready(function() {-->
-<!--			$(window).load(function() {-->
-<!--				var temp = document.getElementById('testItem');-->
-<!--				if(temp.tagName == 'VIDEO')-->
-<!--				{-->
-<!--					temp.addEventListener('ended',endVideo,false);-->
-<!--					temp.play();-->
-<!--				}-->
-<!--			});-->
-<!--		});-->
-<!---->
-<!--		function endVideo() {-->
-<!--			window.location.href = "testMOS.php";-->
-<!--		}-->
-<!---->
-<!--		jQuery(document).ready(function($) {-->
-<!---->
-<!--			if (window.history && window.history.pushState) {-->
-<!--				window.history.pushState('forward', null, './#forward');-->
-<!--				$(window).on('popstate', function() {-->
-<!--					window.location.href = "testCut.php";-->
-<!--				});-->
-<!--			}-->
-<!--		});-->
-<!--						-->
-<!--	</script>-->
-		
+    });
+</script>
 <body>
-<!--<span id="pvs_duration"></span>-->
 <div style="position: relative; width: 100%; height: 100%;">
     <div  id="con" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
         <?php
