2ce12b596653f154addfe87978926fec2f9dd1ed
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-09-05 22:39:22 +0200

    Update testCut.php

--- testCut.php
+++ testCut.php
@@ -5,13 +5,20 @@
     <link rel="stylesheet" href="css/style.css" type="text/css"/>
 </head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<!--<script>-->
+<!--    function time() {-->
+<!--        document.getElementById('com').innerHTML = localStorage.getItem('com');-->
+<!--    }-->
+<!--    setInterval(time,1);-->
+<!--</script>-->
 <body>
 <?php
 session_start();
 ?>
 <section class="container">
     <div class="login">
-        <h3 id="com" style="margin-bottom: 5px;">The test is interrupted</h3>
+<!--        <h3 id="com" style="margin-bottom: 5px;">The test is interrupted</h3>-->
+        <h3 id="com" style="margin-bottom: 5px;"><?php echo $_GET['com'] ?></h3>
         <form method="post" action="indexReturn.php">
             <p class="submit"><input type="submit" name="commit" value="Return"></p>
         </form>
