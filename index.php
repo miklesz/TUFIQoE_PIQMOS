5618de8e63875c1da4913c12eae446d0f8c6dec5
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-10-03 14:00:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -39,23 +39,38 @@
         });
     </script>
 <body>
-<section class="container">
-    <div class="login">
-        <h1 style="	margin-bottom: 5px; margin-top: 5px;"  id="loginCom">Login to the test system</h1>
-        <?php
-        echo $_GET['PROLIFIC_PID'].'<br>';
-        echo $_GET['STUDY_ID'].'<br>';
-        echo $_GET['SESSION_ID'].'<br>';
-        ?>
-        <form method="post" action="text_conf.php">
-            <p><label for="inputPlaceHolder"></label><input type="number" name="id_user" value="" placeholder="Id number" min="0" id="inputPlaceHolder"></p>
-            <label for="soflow"></label><select name="countries" class="countries" id="soflow" onchange="onLanguageChange()">
-                <option value="eng" data-title="English" name="language">English</option>
-                <!--	<option value="pl"  data-title="Polski" name="language">Polski</option>-->
-            </select>
-            <p class="submit"><input type="submit" name="commit" value="Login"></p>
-        </form>
-    </div>
-</section>
+<table style="width:100%">
+    <tr>
+        <td style="width:33%"></td>
+        <td style="width:34%">
+            <section class="container">
+                <div class="login">
+                    <h1 style="	margin-bottom: 5px; margin-top: 5px;"  id="loginCom">Login to the test system</h1>
+                    <?php
+                    echo 'Debug:<br>';
+                    echo 'PROLIFIC_PID = '.$_GET['PROLIFIC_PID'].'<br>';
+                    echo 'STUDY_ID = '.$_GET['STUDY_ID'].'<br>';
+                    echo 'SESSION_ID = '.$_GET['SESSION_ID'].'<br>';
+                    ?>
+                    <form method="post" action="text_conf.php">
+                        <p>
+                            <label for="inputPlaceHolder"></label>
+            <!--                <input type="number" name="id_user" value="ABC" placeholder="Id number" min="0" id="inputPlaceHolder">-->
+                            <input type="text" name="id_user" value="<?php echo $_GET['SESSION_ID'];?>" min="0" id="inputPlaceHolder">
+                        </p>
+                        <label for="soflow"></label><select name="countries" class="countries" id="soflow" onchange="onLanguageChange()">
+                            <option value="eng" data-title="English" name="language">English</option>
+                            <!--	<option value="pl"  data-title="Polski" name="language">Polski</option>-->
+                        </select>
+                        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+                    </form>
+                </div>
+            </section>
+        </td>
+        <td style="width:33%; vertical-align: top; text-align: right;">
+            <img src="https://uploads-ssl.webflow.com/5ed38b28a553da56c8e4bb1e/62a75e1ee5dc8339085b165e_norwayGrants.png">
+        </td>
+    </tr>
+</table>
 </body>
 </html>
