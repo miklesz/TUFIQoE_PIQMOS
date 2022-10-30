f9b13748540cb078a68344df4fe01d6291488e3b
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-10-30 23:17:02 +0100

    Update text_conf.php

--- text_conf.php
+++ text_conf.php
@@ -13,7 +13,7 @@ if( !$id_user || !$lan){
 	exit;
 }
 else {
-	echo 'All data correct';
+	echo 'All data correct<br><br>';
 //	exit;
 	// JN: The code below looks like some bad practices
 	session_start();
@@ -44,54 +44,84 @@ else {
         header( "Location: testCut.php?com=User already exists" );
         exit();
 	}
+
     file_put_contents(
         'results/'.$id_user.'.csv',
-        'id_user,pvs,pvs_duration,eval_id,eval_item,answer,duration,quest,correct,timestamp'.PHP_EOL
+        'id_user,order_id,timestamp,src_id_src,hrc,pvs,submitted_mos,mos_duration,question,submitted_answer,real_answer,answer_duration'.PHP_EOL
     );
-    $src_table = file('config/src.csv');
-    array_shift($src_table);
-    print_r($src_table);
-    echo '<br>';
-    shuffle($src_table);
-    print_r($src_table);
-    echo '<br>';
-    $hrc = file('config/hrc.txt');
-    print_r($hrc);
-    echo '<br>';
-    $ext = file_get_contents('config/ext.txt');
-    echo $ext.'<br>';
-    $pvs_array = array();
-    $quest_array = array();
-    $correct_array = array();
-    $eval_array = array();
-    foreach ($src_table as $src_row){
-        $src_row_array = explode(',', $src_row);
-        print_r($src_row_array);
-        echo '<br>';
-        $quest_array[] = $src_row_array[1];
-        $correct_array[] = $src_row_array[2];
-        if ($src_row_array[1]) {
-            $eval_array[] = 'quest:'.$src_row_array[3];
-        } else {
-            $eval_array[] = $src_row_array[3];
-        }
-        $current_hrc_key = array_rand($hrc);
-        $current_pvs = trim($src_row_array[0]).'_'.trim($hrc[$current_hrc_key]).'.'.trim($ext);
-        $pvs_array[] = $current_pvs;
-//        echo "src_line = $src_row, current_src = $current_src, current_pvs = $current_pvs, current_quest_correct = $current_quest_correct<br>";
-    }
-    print_r($pvs_array);
-    echo '<br>';
-    print_r($quest_array);
-    echo '<br>';
-    print_r($correct_array);
-    echo '<br>';
-    print_r($eval_array);
-    echo '<br>';
-    $_SESSION['pvs_array'] = $pvs_array;
-    $_SESSION['quest_array'] = $quest_array;
-    $_SESSION['correct_array'] = $correct_array;
-    $_SESSION['eval_array'] = $eval_array;
+
+    $order_id = file('config/max_order.txt')[0];
+    file_put_contents('config/max_order.txt',$order_id+1);
+    echo 'order_id = ';
+    print_r($order_id);
+    echo '<br><br>';
+    $_SESSION['order_id'] = $order_id;
+
+    $orders = file('config/orders.csv');
+    $srcs = explode(',', $orders[0]);
+    $order = explode(',', $orders[$order_id+1]);
+    array_shift($srcs);
+    array_shift($order);
+    $hrcs = array_combine($srcs, $order);
+    echo 'hrcs = ';
+    print_r($hrcs);
+    echo '<br><br>';
+    $_SESSION['hrcs'] = $hrcs;
+
+    shuffle($srcs);
+    echo 'srcs = ';
+    print_r($srcs);
+    echo '<br><br>';
+    $_SESSION['srcs'] = $srcs;
+
+//    $src_table = file('config/src.csv');
+//    array_shift($src_table);
+//    print_r($src_table);
+//    echo '<br>';
+//    shuffle($src_table);
+//    print_r($src_table);
+//    echo '<br>';
+//
+//    echo 'hrc:<br>';
+//    $hrc = file('config/hrc.txt');
+//    print_r($hrc);
+//    echo '<br>';
+//
+//    $ext = file_get_contents('config/ext.txt');
+//    echo $ext.'<br>';
+//    $pvs_array = array();
+//    $quest_array = array();
+//    $correct_array = array();
+//    $eval_array = array();
+//    foreach ($src_table as $src_row){
+//        $src_row_array = explode(',', $src_row);
+//        print_r($src_row_array);
+//        echo '<br>';
+//        $quest_array[] = $src_row_array[1];
+//        $correct_array[] = $src_row_array[2];
+//        if ($src_row_array[1]) {
+//            $eval_array[] = 'quest:'.$src_row_array[3];
+//        } else {
+//            $eval_array[] = $src_row_array[3];
+//        }
+//        $current_hrc_key = array_rand($hrc);
+//        $current_pvs = trim($src_row_array[0]).'_'.trim($hrc[$current_hrc_key]).'.'.trim($ext);
+//        $pvs_array[] = $current_pvs;
+////        echo "src_line = $src_row, current_src = $current_src, current_pvs = $current_pvs, current_quest_correct = $current_quest_correct<br>";
+//    }
+//    print_r($pvs_array);
+//    echo '<br>';
+//    print_r($quest_array);
+//    echo '<br>';
+//    print_r($correct_array);
+//    echo '<br>';
+//    print_r($eval_array);
+//    echo '<br>';
+//    $_SESSION['pvs_array'] = $pvs_array;
+//    $_SESSION['quest_array'] = $quest_array;
+//    $_SESSION['correct_array'] = $correct_array;
+//    $_SESSION['eval_array'] = $eval_array;
+
 //    exit();
 //    exit('Randomisation exit');
     header( "Location: testStart.php" );
