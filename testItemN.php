82308c3025af3717e61225e0f347bbaed3832f32
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-10-30 23:16:58 +0100

    Update testItemN.php

--- testItemN.php
+++ testItemN.php
@@ -18,7 +18,7 @@
     $(window).load(function() {
         window.setTimeout(function(){
             window.location.href = "testMOS.php";
-        }, 3000);
+        }, 30000);
     });
 
     const start = Date.now();
@@ -42,16 +42,48 @@
 //        ini_set('display_errors', '1');
 //        ini_set('display_startup_errors', '1');
         session_start();
-        $pvs_array = $_SESSION['pvs_array'];
-//            print_r($pvs);
-//            echo '<br>';
-        $pvs_no = $_SESSION['pvs_no'];
-//            echo 'pvs_no='.$pvs_no.'<br>';
-        $id_user = $_SESSION['id_user'];
+
+        $srcs = $_SESSION['srcs'];
+        echo '$srcs = ';
+        print_r($srcs);
+        echo '<br><br>';
+
+        $src_id = $_SESSION['src_id'];
+        echo '$src_id = ';
+        print_r($src_id);
+        echo '<br><br>';
+
+        $src = $srcs[$src_id];
+        echo '$src = ';
+        print_r($src);
+        echo '<br><br>';
+
+        $hrcs = $_SESSION['hrcs'];
+        echo '$hrcs = ';
+        print_r($hrcs);
+        echo '<br><br>';
+
+        $hrc = $hrcs[$src];
+        echo '$hrc = ';
+        print_r($hrc);
+        echo '<br><br>';
+
+        $pos = strrpos($src, '.');
+        echo '$pos = ';
+        print_r($pos);
+        echo '<br><br>';
+
+        $pvs = substr($src, 0, $pos).'_'.$hrc.substr($src, $pos);
+        echo '$pvs = ';
+        print_r($pvs);
+        echo '<br><br>';
+
+//        $id_user = $_SESSION['id_user'];
 //            echo 'id_user='.$id_user.'<br>';
 //        $_SESSION['pvs_time'] = microtime(true);
-        $_SESSION['eval_id'] = 0;
-        echo '<a href="testMOS.php"><img src="Data/'.$pvs_array[$pvs_no].'" alt=""></a>';
+//        $_SESSION['eval_id'] = 0;
+//        echo '<a href="testMOS.php"><img src="Data/'.$pvs_array[$pvs_no].'" alt=""></a>';
+//        echo '<img src="Data/'.$pvs_array[$pvs_no].'" alt="">';
         ?>
     </div>
 </div>
