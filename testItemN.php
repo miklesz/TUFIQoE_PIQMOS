8541cda00dd64c8e66711b22841f94698449f04a
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-10-31 17:49:31 +0100

    Update testItemN.php

--- testItemN.php
+++ testItemN.php
@@ -8,22 +8,6 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 <script type="text/javascript" >
-
-    function time(){
-        // document.getElementById('pvs_duration').innerHTML = (Date.now() - start).toString();
-        localStorage.setItem('pvs_duration', (Date.now() - start).toString());
-        // document.getElementById('pvs_duration').innerHTML = localStorage.getItem('pvs_duration');
-    }
-
-    $(window).load(function() {
-        window.setTimeout(function(){
-            window.location.href = "testMOS.php";
-        }, 30000);
-    });
-
-    const start = Date.now();
-    setInterval(time, 1);
-
     jQuery(document).ready(function($) {
         if (window.history && window.history.pushState) {
             window.history.pushState('forward', null, './#forward');
@@ -44,48 +28,72 @@
         session_start();
 
         $srcs = $_SESSION['srcs'];
-        echo '$srcs = ';
-        print_r($srcs);
-        echo '<br><br>';
+//        echo '$srcs = ';
+//        print_r($srcs);
+//        echo '<br><br>';
 
         $src_id = $_SESSION['src_id'];
-        echo '$src_id = ';
-        print_r($src_id);
-        echo '<br><br>';
+//        echo '$src_id = ';
+//        print_r($src_id);
+//        echo '<br><br>';
 
         $src = $srcs[$src_id];
-        echo '$src = ';
-        print_r($src);
-        echo '<br><br>';
+//        echo '$src = ';
+//        print_r($src);
+//        echo '<br><br>';
 
         $hrcs = $_SESSION['hrcs'];
-        echo '$hrcs = ';
-        print_r($hrcs);
-        echo '<br><br>';
+//        echo '$hrcs = ';
+//        print_r($hrcs);
+//        echo '<br><br>';
 
         $hrc = $hrcs[$src];
-        echo '$hrc = ';
-        print_r($hrc);
-        echo '<br><br>';
+//        echo '$hrc = "';
+//        print_r($hrc);
+//        echo '"<br><br>';
 
         $pos = strrpos($src, '.');
-        echo '$pos = ';
-        print_r($pos);
-        echo '<br><br>';
+//        echo '$pos = ';
+//        print_r($pos);
+//        echo '<br><br>';
 
         $pvs = substr($src, 0, $pos).'_'.$hrc.substr($src, $pos);
-        echo '$pvs = ';
-        print_r($pvs);
-        echo '<br><br>';
+//        echo '$pvs = ';
+//        print_r($pvs);
+//        echo '<br><br>';
 
 //        $id_user = $_SESSION['id_user'];
 //            echo 'id_user='.$id_user.'<br>';
-//        $_SESSION['pvs_time'] = microtime(true);
 //        $_SESSION['eval_id'] = 0;
 //        echo '<a href="testMOS.php"><img src="Data/'.$pvs_array[$pvs_no].'" alt=""></a>';
-//        echo '<img src="Data/'.$pvs_array[$pvs_no].'" alt="">';
+
+        if (substr($pvs, strrpos($pvs, '.'))=='.mp4'){
+            echo '<video autoplay muted>';
+            echo '    <source src="Data/'.$pvs.'" type="video/mp4">';
+            echo '</video>';
+        }else{
+            echo '<img src="Data/'.$pvs.'" alt="">';
+        }
+//        $_SESSION['pvs_start_time'] = microtime(true);
         ?>
     </div>
 </div>
 </body>
 </html>
+<script type="text/javascript" >
+    function time(){
+        // document.getElementById('pvs_duration').innerHTML = (Date.now() - start).toString();
+        localStorage.setItem('pvs_duration', (Date.now() - start).toString());
+        // document.getElementById('pvs_duration').innerHTML = localStorage.getItem('pvs_duration');
+    }
+
+    $(window).load(function() {
+        window.setTimeout(function(){
+            window.location.href = "testMOS.php";
+        }, 3000);
+    });
+
+    const start = Date.now();
+    setInterval(time, 1);
+</script>
+
