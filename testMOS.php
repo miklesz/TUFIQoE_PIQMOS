233945f70faebc62f1faab75f1eae7e2640a5203
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-11-16 22:08:01 +0100

    Update testMOS.php

--- testMOS.php
+++ testMOS.php
@@ -29,104 +29,55 @@
 </script>
 <body onload="checkMos()">
 <div class="answer">
-    <form method="post" action="next_page.php" style = "margin-bottom: 0;">
-<!--        <label for="pvs_duration">-->
-<!--            <input id="pvs_duration" type="hidden" name="pvs_duration" value="">-->
-<!--        </label>-->
-        <label for="mos_duration">
-            <input id="mos_duration" type="hidden" name="mos_duration" value="">
-        </label>
-        <label for="answer_duration">
-            <input id="answer_duration" type="hidden" name="answer_duration" value="">
+    <form id="form" method="post" action="next_page.php" style = "margin-bottom: 0;">
+        <label for="duration">
+            <input id="duration" type="hidden" name="duration" value="">
         </label>
         <?php
         session_start();
-//        $pvs_duration = microtime(true) - $_SESSION['pvs_time'];
-//        echo '$pvs_duration = ';
-//        echo microtime(true) - $_SESSION['pvs_start_time'];
-//        echo '<br><br>';
         $button = 'Next';
-//        $pre_time = microtime(true);
-//        $pvs_duration = microtime(true) - $_SESSION['pvs_time'];
-        $quest_array = $_SESSION['quest_array'];
-        $correct_array = $_SESSION['correct_array'];
-        $eval_array = $_SESSION['eval_array'];
-        $pvs_no = $_SESSION['pvs_no'];
+
+
+//        $quest_array = $_SESSION['quest_array'];
+//        $correct_array = $_SESSION['correct_array'];
+//        $eval_array = $_SESSION['eval_array'];
+//        $pvs_no = $_SESSION['pvs_no'];
         $buttons = 0;
-        $quest = $quest_array[$pvs_no];
+//        $quest = $quest_array[$pvs_no];
 //        echo "<input id='pre_time' type='hidden' name='pre_time' value='$pre_time'>";
 //        echo "<input id='pvs_duration' type='hidden' name='pvs_duration' value='$pvs_duration'>";
-        $eval = trim($eval_array[$pvs_no]);
-        $eval_exploded = explode(':', $eval);
+//        $eval = trim($eval_array[$pvs_no]);
+//        $eval_exploded = explode(':', $eval);
 //        echo "$eval<br>";
 //        print_r($eval_exploded);
 //        print_r("$quest_array<br>");
-        $eval_id = $_SESSION['eval_id'];
-        $eval_item = $eval_exploded[$eval_id];
-        if ($eval_item == 'random') {
-            $eval_item = array('mos', 'arousal', 'valence', 'approach/avoidance')[rand(0, 3)];
-        }
+//        $eval_id = $_SESSION['eval_id'];
+//        $eval_item = $eval_exploded[$eval_id];
+//        if ($eval_item == 'random') {
+//            $eval_item = array('mos', 'arousal', 'valence', 'approach/avoidance')[rand(0, 3)];
+//        }
 //        echo '<br>';
 //        echo "eval = $eval<br>";
 //        echo 'eval_exploded = ';
 //        print_r($eval_exploded);
 //        echo '<br>';
 //        echo "eval_id = $eval_id, eval_item = $eval_item<br>";
-        echo "<input id='pre_time' type='hidden' name='eval_item' value='$eval_item'>";
-        echo "<input id='pre_time' type='hidden' name='eval_id' value='$eval_id'>";
-        if ($eval_item == 'quest'){
+        $question = $_SESSION['question'];
+        $quest = 'This is question';
+        echo "<input id='question' type='hidden' name='question' value='$question'>";
+        if ($question){
             echo "<h3>$quest</h3>";
             echo '<input id="pageNext" type="hidden" name="page" value="answer">';
             echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='true'> true<br><br></label>";
             echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='false'> false<br><br></label>";
-        }
-        if ($eval_item == 'mos'){
+        } else {
             echo '<h3>Assess quality of material displayed</h3>';
             echo '<input id="pageNext" type="hidden" name="page" value="answer">';
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='1'> bad<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='2'> poor<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='3'> fair<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='4'> good<br><br></label>";
             echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='5'> excellent<br><br></label>";
-        }
-        if ($eval_item == 'arousal'){
-            echo '<h3>Assess arousal of material displayed</h3>';
-            echo '<input id="pageNext" type="hidden" name="page" value="answer">';
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='1'> 1<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='2'> 2<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='3'> 3<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='4'> 4<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='5'> 5<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='6'> 6<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='7'> 7<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='8'> 8<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='9'> 9<br><br></label>";
-        }
-        if ($eval_item == 'valence'){
-            echo '<h3>Assess valence of material displayed</h3>';
-            echo '<input id="pageNext" type="hidden" name="page" value="answer">';
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='1'> 1<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='2'> 2<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='3'> 3<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='4'> 4<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='5'> 5<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='6'> 6<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='7'> 7<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='8'> 8<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='9'> 9<br><br></label>";
-        }
-        if ($eval_item == 'approach/avoidance'){
-            echo '<h3>Assess approach/avoidance of material displayed</h3>';
-            echo '<input id="pageNext" type="hidden" name="page" value="answer">';
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='1'> 1<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='2'> 2<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='3'> 3<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='4'> 4<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='5'> 5<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='6'> 6<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='7'> 7<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='8'> 8<br><br></label>";
-            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='9'> 9<br><br></label>";
+            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='4'> good<br><br></label>";
+            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='3'> fair<br><br></label>";
+            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='2'> poor<br><br></label>";
+            echo "<label><input type='radio' name='answer' onClick='checkMos($buttons);' value='1'> bad<br><br></label>";
         }
         echo '<input id="nextButton" type="submit" value="Next" disabled>';
         ?>
@@ -135,16 +86,10 @@
 </body>
 </html>
 <script type="text/javascript">
-    // const start = Date.now();
-    // function time(){
-    //     document.getElementById('pvs_duration').value = localStorage.getItem('pvs_duration');
-    //     document.getElementById('quest_duration').value = (Date.now() - start).toString();
-    // }
-    // setInterval(time,1);
-
-    function time(){
-        localStorage.setItem('duration', (Date.now() - start).toString());
-    }
     const start = Date.now();
-    setInterval(time, 1);
+    document.getElementById('nextButton').onclick = (event) => {
+        event.preventDefault();
+        document.getElementById('duration').value = (Date.now() - start).toString()
+        document.getElementById('form').submit()
+    }
 </script>
