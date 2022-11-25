d965221546b2c0ce1b5b659a980443230da91fb8
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-11-25 17:21:06 +0100

    Update testItemN.php

--- testItemN.php
+++ testItemN.php
@@ -20,23 +20,49 @@
     });
 </script>
 <body>
+<?php
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+
+    session_start();
+
+    $srcs = $_SESSION['srcs'];
+    $src_id = $_SESSION['src_id'];
+    $src = $srcs[$src_id];
+    $hrcs = $_SESSION['hrcs'];
+    $hrc = $hrcs[$src];
+    $pos = strrpos($src, '.');
+    $pvs = substr($src, 0, $pos).'_'.$hrc.substr($src, $pos);
+    $experiment = $_SESSION['experiment'];
+    $group = $_SESSION['group'];
+    $pvs = $src;
+//    print_r($srcs);
+//    echo '<br>';
+//    print_r($src_id);
+//    echo '<br>';
+//    echo '"'.$pvs.'"<br>';
+//    echo 'experiment = "'.$experiment.'"<br>';
+//    echo 'group = "'.$group.'"<br>';
+?>
 <div style="position: relative; width: 100%; height: 100%;">
     <div  id="con" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
         <?php
-        ini_set('display_errors', '1');
-        ini_set('display_startup_errors', '1');
-
-        session_start();
-
-        $srcs = $_SESSION['srcs'];
-        $src_id = $_SESSION['src_id'];
-        $src = $srcs[$src_id];
-        $hrcs = $_SESSION['hrcs'];
-        $hrc = $hrcs[$src];
-        $pos = strrpos($src, '.');
-        $pvs = substr($src, 0, $pos).'_'.$hrc.substr($src, $pos);
-        $experiment = $_SESSION['experiment'];
+//        ini_set('display_errors', '1');
+//        ini_set('display_startup_errors', '1');
 //
+//        session_start();
+//
+//        $srcs = $_SESSION['srcs'];
+//        $src_id = $_SESSION['src_id'];
+//        $src = $srcs[$src_id];
+//        $hrcs = $_SESSION['hrcs'];
+//        $hrc = $hrcs[$src];
+//        $pos = strrpos($src, '.');
+//        $pvs = substr($src, 0, $pos).'_'.$hrc.substr($src, $pos);
+//        $experiment = $_SESSION['experiment'];
+
+
+        //
 //        echo '$srcs = '.print_r($srcs, true).'<br>';
 //        echo '$src_id = '.print_r($src_id, true).'<br>';
 //        echo '$src = '.print_r($src, true).'<br>';
@@ -46,19 +72,21 @@
 //        echo '$pvs = '.print_r($pvs, true).'<br>';
 //        echo '$experiment = '.print_r($experiment, true).'<br>';
 
-        if ($experiment) {
-            $pvs = $src;
-        }
+//        if ($experiment) {
+//            $pvs = $src;
+//        }
 
-        if (substr($pvs, strrpos($pvs, '.'))=='.mp4'){
-//            echo '"'.$pvs.'"<br>';
+//        if (substr($pvs, strrpos($pvs, '.'))=='.jpg'){
+////            echo '<a href="testMOS.php"><img src="Data/'.$pvs.'" alt="" id="image" width="50%"></a>';
+//            echo '<img src="Data/'.$pvs.'" alt="" id="image">';
+//        }else{
+//            echo '<a href="testMOS.php">';
+//            echo '<video autoplay muted width="150%">';
             echo '<video autoplay muted>';
             echo '    <source src="Data_experiment/'.$pvs.'" type="video/mp4" id="image">';
             echo '</video>';
-        }else{
-//            echo '<img src="Data/'.$pvs.'" alt="" id="image" width="50%">';
-            echo '<img src="Data/'.$pvs.'" alt="" id="image">';
-        }
+//            echo '</a>';
+//        }
 //        $_SESSION['pvs_start_time'] = microtime(true);
         ?>
     </div>
