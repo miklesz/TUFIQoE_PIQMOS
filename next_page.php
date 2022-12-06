8485f080d95a849708b4fdb0fdadbf079ca9e891
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-12-06 18:45:55 +0100

    Update next_page.php

--- next_page.php
+++ next_page.php
@@ -11,6 +11,11 @@ $ask_question = false;
 $srcs = $_SESSION['srcs'];
 $src_id = $_SESSION['src_id'];
 $src = $srcs[$src_id];
+$hrcs = $_SESSION['hrcs'];
+$hrc = $hrcs[$src];
+$pos = strrpos($src, '.');
+$pvs = substr($src, 0, $pos).'_'.$hrc.substr($src, $pos);
+
 $question = $_SESSION['question'];
 $real_answer = $_SESSION['real_answer'];
 
@@ -40,13 +45,61 @@ if ($question){
     }
 }
 
-# Either come back with question or store to file
+//NAPS parameters
+$valence = '';
+$av_ap = '';
+$arousal = '';
+$luminance = '';
+$contrast = '';
+$jpeg_size80 = '';
+$labl = '';
+$laba = '';
+$labb = '';
+$entropy = '';
+$naps_file = file('config/13428_2013_379_MOESM1_ESM.csv');
+foreach ($naps_file as $naps_line) {
+    $naps_elements = explode(";", $naps_line);
+    if ($naps_elements[0].'.jpg' == $src) {
+        $valence = str_replace(",", ".", $naps_elements[17]);
+        $av_ap = str_replace(",", ".", $naps_elements[19]);
+        $arousal = str_replace(",", ".", $naps_elements[21]);
+        $luminance = str_replace(",", ".", $naps_elements[25]);
+        $contrast = str_replace(",", ".", $naps_elements[26]);
+        $jpeg_size80 = str_replace(",", ".", $naps_elements[27]);
+        $labl = str_replace(",", ".", $naps_elements[28]);
+        $laba = str_replace(",", ".", $naps_elements[29]);
+        $labb = str_replace(",", ".", $naps_elements[30]);
+        $entropy = str_replace(",", ".", $naps_elements[31]);
+    }
+}
+
+//PVS parameters
+$desired_metric = '';
+$achieved_metric = '';
+$distance_index = '';
+$distance = '';
+$pvs_file = file('config/pvs_stats_old.csv');
+foreach ($pvs_file as $pvs_line) {
+    $pvs_elements = explode(",", $pvs_line);
+    echo substr($pvs_elements[0], 0, -4).'_'.$pvs_elements[1].'.jpg'.'<br>';
+    echo $pvs.'<br>';
+    if ($pvs_elements[0] == $src) {
+        $desired_metric = $pvs_elements[2];
+        $achieved_metric = $pvs_elements[3];
+        $distance_index = $pvs_elements[4];
+        $distance = $pvs_elements[5];
+    }
+}
+
+//Either come back with question or store to file
 if ($ask_question) {
     header("Location: testMOS.php");
 //    echo '<a href="testMOS.php">testMOS.php</a>';
 } else {
     //'id_user,order_id,timestamp,src_id,hrc,pvs,submitted_mos,mos_duration,question,submitted_answer,real_answer,answer_duration'.PHP_EOL
-    $id_user = $_SESSION['id_user'];
+    $prolific_pid = $_SESSION['prolific_pid'];
+    $study_id = $_SESSION['study_id'];
+    $session_id = $_SESSION['session_id'];
     $order_id = $_SESSION['order_id'];
     date_default_timezone_set('Europe/Warsaw');
     $timestamp = date('Y-m-d H:i:s');
@@ -59,7 +112,7 @@ if ($ask_question) {
     $pos = strrpos($src, '.');
     $pvs = substr($src, 0, $pos).'_'.$hrc.substr($src, $pos);
 
-    echo '$id_user = '.$id_user.'<br>';
+    echo '$session_id = '.$session_id.'<br>';
     echo '$order_id = '.$order_id.'<br>';
     echo '$timestamp = '.$timestamp.'<br>';
     echo '$src_id = '.$src_id.'<br>';
@@ -73,8 +126,8 @@ if ($ask_question) {
     echo '$answer_duration = '.$answer_duration.'<br>';
 
     file_put_contents(
-        'results/'.$id_user.'.csv',
-        "$id_user,$order_id,$timestamp,$src_id,$hrc,$pvs,$submitted_mos,$mos_duration,$question,$submitted_answer,$real_answer,$answer_duration".PHP_EOL,
+        'results/'.$session_id.'.csv',
+        "$prolific_pid,$study_id,$session_id,$order_id,$timestamp,$src_id,$hrc,$pvs,$submitted_mos,$mos_duration,$question,$submitted_answer,$real_answer,$answer_duration,$valence,$av_ap,$arousal,$luminance,$contrast,$jpeg_size80,$labl,$laba,$labb,$entropy,$desired_metric,$achieved_metric,$distance_index,$distance".PHP_EOL,
         FILE_APPEND | LOCK_EX
     );
 
@@ -85,87 +138,10 @@ if ($ask_question) {
     $_SESSION['src_id'] += 1;
     $src_id = $_SESSION['src_id'];
     if ($src_id == count($srcs)) {
-        header("Location: testCut.php?com=The test is done");
-//        echo '<a href="testCut.php?com=The test is done">testCut.php?com=The test is done</a>';
+//        header("Location: testCut.php?com=The test is done");
+        echo '<a href="testCut.php?com=The test is done">testCut.php?com=The test is done</a>';
     } else{
-        header("Location: testItemN.php");
-//        echo '<a href="testItemN.php">testItemN.php</a>';
+//        header("Location: testItemN.php");
+        echo '<a href="testItemN.php">testItemN.php</a>';
     }
 }
-
-//exit;
-//
-//$id_user = 	$_SESSION['id_user'];
-//$mos = $_POST['answer'];
-//$pvs_array = $_SESSION['pvs_array'];
-//$quest_array = $_SESSION['quest_array'];
-//$correct_array = $_SESSION['correct_array'];
-//$eval_array = $_SESSION['eval_array'];
-//$pvs_no = $_SESSION['pvs_no'];
-//$current_pvs = $pvs_array[$pvs_no];
-//$quest_duration = microtime(true) - $_POST['pre_time'];
-//$quest_duration = $_POST['quest_duration'];
-//$pvs_duration = $_POST['pvs_duration'];
-//$eval_item = $_POST['eval_item'];
-//$eval_id = $_POST['eval_id'];
-//if (array_key_exists("answer", $_POST)) {
-//    $answer = $_POST['answer'];
-//} else {
-//    $answer = null;
-//}
-//if ($eval_item == 'quest') {
-//    $quest = $quest_array[$pvs_no];
-//    $correct = $correct_array[$pvs_no];
-//} else {
-//    $quest = '';
-//    $correct = '';
-//}
-//
-//print_r($_SESSION);
-//echo '<br>';
-//print_r($_POST);
-//echo '<br>';
-////exit();
-////echo '<br>';
-////echo 'id_user='.$id_user.'<br>';
-////echo 'pvs_no='.$pvs_no.'<br>';
-////echo 'current_pvs='.$current_pvs.'<br>';
-////echo 'mos='.$mos.'<br>';
-////$timestamp = str_replace(',', '', date('r', time()));
-//date_default_timezone_set('Europe/Warsaw');
-//$timestamp = date('Y-m-d H:i:s');
-//
-////'id_user,pvs,pvs_duration,eval_id,eval_item,answer,duration,quest,correct,timestamp'.PHP_EOL
-//file_put_contents(
-//    'results/'.$id_user.'.csv',
-//    "$id_user,$current_pvs,$pvs_duration,$eval_id,$eval_item,$answer,$quest_duration,$quest,$correct,$timestamp".PHP_EOL,
-//    FILE_APPEND | LOCK_EX
-//);
-////exit();
-//
-//$eval = trim($eval_array[$pvs_no]);
-//$eval_exploded = explode(':', $eval);
-//$eval_count = count($eval_exploded);
-//
-//echo "eval = $eval<br>";
-//echo 'eval_exploded = ';
-//print_r($eval_exploded);
-//echo '<br>';
-//echo "eval_id = $eval_id<br>";
-//echo "eval_count = $eval_count<br>";
-//
-//if ($eval_id < $eval_count - 1) {
-//    echo("$eval_id < $eval_count - 1<br>");
-//    $_SESSION['eval_id'] += 1;
-//    header( "Location: testMOS.php" );
-//} else {
-//    $_SESSION['pvs_no'] += 1;
-//    $pvs_no = $_SESSION['pvs_no'];
-////    echo 'pvs_no='.$pvs_no.'<br>';
-////    echo count($pvs).'<br>';
-//    if ($pvs_no==count($pvs_array)) {
-//        header( "Location: testCut.php?com=The test is done" );
-//    } else{
-//        header( "Location: testItemN.php" );
-//    }
-//}
